6fda990423aec1c89c0ecd2a03dc1f7ecd40507c
jhall1990 <jacobhall90@gmail.com>
2019-01-11 13:30:01 -0500

    at 95% now

--- src/Client.java
+++ src/Client.java
@@ -6,25 +6,14 @@ public class Client {
     public static void main(String[] args) {
 
          // read the n points from a file
-//         In in = new In(args[0]);
-//         int n = in.readInt();
-//         Point[] points = new Point[n];
-//         for (int i = 0; i < n; i++) {
-//             int x = in.readInt();
-//             int y = in.readInt();
-//             points[i] = new Point(x, y);
-//         }
-
-        Point[] points = new Point[9];
-        points[0] = new Point(9000, 9000);
-        points[1] = new Point(8000, 8000);
-        points[2] = new Point(7000, 7000);
-        points[3] = new Point(6000, 6000);
-        points[4] = new Point(5000, 5000);
-        points[5] = new Point(4000, 4000);
-        points[6] = new Point(3000, 3000);
-        points[7] = new Point(2000, 2000);
-        points[8] = new Point(1000, 1000);
+         In in = new In(args[0]);
+         int n = in.readInt();
+         Point[] points = new Point[n];
+         for (int i = 0; i < n; i++) {
+             int x = in.readInt();
+             int y = in.readInt();
+             points[i] = new Point(x, y);
+         }
 
         // draw the points
         StdDraw.enableDoubleBuffering();
@@ -36,14 +25,21 @@ public class Client {
         StdDraw.show();
 
         // print and draw the line segments
+        BruteCollinearPoints brute = new BruteCollinearPoints(points);
+
+        System.out.println("Brute:");
+        for (LineSegment segment : brute.segments()) {
+            StdOut.println(segment);
+        }
+
         FastCollinearPoints fast = new FastCollinearPoints(points);
-//        BruteCollinearPoints brute = new BruteCollinearPoints(points);
 
         System.out.println("Fast:");
         for (LineSegment segment : fast.segments()) {
             StdOut.println(segment);
             segment.draw();
         }
+        System.out.println("\nCount: " + fast.numberOfSegments());
 
         StdDraw.show();
     }
--- src/FastCollinearPoints.java
+++ src/FastCollinearPoints.java
@@ -6,19 +6,19 @@ public class FastCollinearPoints {
     private final ArrayList<Point[]> segments;
 
     public FastCollinearPoints(Point[] points) {
-        // todo remove slope object. Sort each point based on a a passed origin point.
-        // todo check if the segment you are adding has already been added.
-        // todo should be able to check the slope of a point on the line with the
-        // todo existing segment.
-        
+        if (points == null) {
+            throw new IllegalArgumentException("Points array must not be null");
+        } else if (points[0] == null) {
+            throw new IllegalArgumentException("No point in points array can be null");
+        }
+
         // finds all line segments containing 4 or more points
         segments = new ArrayList<>();
 
         for (int i = 0; i < points.length - 1; i++) {
             Point[] subArray = Arrays.copyOfRange(points, i + 1, points.length);
-            SlopeObject[] slopes = createSlopeObjects(points[i], subArray);
-            sort(slopes);
-            findCollinear(slopes);
+            sort(points[i], subArray);
+            findCollinear(points[i], subArray);
         }
 
         lines = new LineSegment[segments.size()];
@@ -38,24 +38,15 @@ public class FastCollinearPoints {
         return Arrays.copyOf(lines, lines.length);
     }
 
-    private SlopeObject[] createSlopeObjects(Point origin, Point[] otherPoints) {
-        // This function creates an array of SlopeObjects that will later be sorted.
-        SlopeObject[] slopes = new SlopeObject[otherPoints.length];
-
-        for (int i = 0; i < otherPoints.length; i++) {
-            slopes[i] = new SlopeObject(origin, otherPoints[i]);
-        }
-
-        return slopes;
-    }
-
-    private void sort(SlopeObject[] a) {
+    private void sort(Point origin, Point[] a) {
         // This function sorts an array of slope objects based on their slopes.
-        SlopeObject[] aux = new SlopeObject[a.length];
-        sort(a, aux, 0, a.length - 1);
+
+        // Create an aux array that is a copy of <a>.
+        Point[] aux = new Point[a.length];
+        sort(origin, a, aux, 0, a.length - 1);
     }
 
-    private void sort(SlopeObject[] a, SlopeObject[] aux, int lo, int hi) {
+    private void sort(Point origin, Point[] a, Point[] aux, int lo, int hi) {
         // Overloaded sort function. Uses recursive merge sort to sort an array
         // of SlopeObjects based on their slope.
         if (hi <= lo) {
@@ -64,13 +55,13 @@ public class FastCollinearPoints {
 
         int mid = lo + (hi - lo) / 2;
 
-        sort(a, aux, lo, mid);
-        sort(a, aux, mid + 1, hi);
-        merge(a, aux, lo, mid, hi);
+        sort(origin, a, aux, lo, mid);
+        sort(origin, a, aux, mid + 1, hi);
+        merge(origin, a, aux, lo, mid, hi);
 
     }
 
-    private void merge(SlopeObject[] a, SlopeObject[] aux, int lo, int mid, int hi) {
+    private void merge(Point origin, Point[] a, Point[] aux, int lo, int mid, int hi) {
         // Merges an array <a> and auxiliary array <aux>.
         for (int k = lo; k <= hi; k++) {
             aux[k] = a[k];
@@ -84,7 +75,7 @@ public class FastCollinearPoints {
                 a[k] = aux[j++];
             } else if (j > hi) {
                 a[k] = aux[i++];
-            } else if (less(aux[j], aux[i])) {
+            } else if (less(origin, aux[j], aux[i])) {
                 a[k] = aux[j++];
             } else {
                 a[k] = aux[i++];
@@ -92,50 +83,86 @@ public class FastCollinearPoints {
         }
     }
 
-    private boolean less(SlopeObject s1, SlopeObject s2) {
-        return s1.slope < s2.slope;
+    private boolean less(Point origin, Point p1, Point p2) {
+        // Get the slope from the origin point to p1 and p2. Then
+        // compare them. Return True if p1's slope is less than p2's.
+        if (origin == null || p1 == null || p2 == null)
+            throw new IllegalArgumentException("No point in points array can be null");
+
+        double p1Slope = origin.slopeTo(p1);
+        double p2Slope = origin.slopeTo(p2);
+        return p1Slope < p2Slope;
     }
 
-    private void findCollinear(SlopeObject[] slopes) {
-        double curSlope = slopes[0].slope;
-        ArrayList<SlopeObject> curSegment = new ArrayList<>();
-        curSegment.add(slopes[0]);
+    private void findCollinear(Point origin, Point[] points) {
+        // This function iterates over each point in the point array and checks
+        // It's slope against the current slope. It builds segments and checks if
+        // they needed to be added by calling addSegment().
+        if (origin == null || points[0] == null)
+            throw new IllegalArgumentException("No point in points array can be null");
 
-        for (int i = 1; i < slopes.length; i++) {
-            if (Double.compare(curSlope, slopes[i].slope) == 0) {
-                curSegment.add(slopes[i]);
-            } else if (curSegment.size() >= 3) {
-                addSegment(curSegment);
-                curSegment = new ArrayList<>();
-                curSegment.add(slopes[i]);
-                curSlope = slopes[i].slope;
+        double curSlope = origin.slopeTo(points[0]);
+        ArrayList<Point> curSegment = new ArrayList<>();
+
+        for (Point p : points) {
+            double slope = origin.slopeTo(p);
+
+            // Check the points slope against the current slope. If it's the same
+            // add the point to the current segment.
+            if (origin.compareTo(p) == 0)
+                throw new IllegalArgumentException("Points array cannot have duplicate entries.");
+            else if (Double.compare(curSlope, slope) == 0) {
+                curSegment.add(p);
+            // If the slope is different and the current segment has more than 3
+            // entries add it to the segment array (if needed). Then create a new
+            // current segment array and add the current point. Then update the
+            // current slope.
             } else {
+                if (curSegment.size() >= 3) {
+                    addSegment(origin, curSegment);
+                }
+
                 curSegment = new ArrayList<>();
-                curSegment.add(slopes[i]);
-                curSlope = slopes[i].slope;
+                curSegment.add(p);
+                curSlope = origin.slopeTo(p);
             }
         }
 
+        // If the last point matches the current slope it won't be added
+        // to the segments array. This is a final check to see if the current
+        // segment is long enough to be added.
         if (curSegment.size() >= 3) {
-            addSegment(curSegment);
+            addSegment(origin, curSegment);
         }
     }
 
-    private void addSegment(ArrayList<SlopeObject> segment) {
-        Point min = segment.get(0).origin;
-        Point max = segment.get(0).origin;
+    private void addSegment(Point origin, ArrayList<Point> segment) {
+        // This function checks to see if the passed segment should be added to the
+        // segments array.
 
-        for (SlopeObject s : segment) {
-            if (min.compareTo(s.p) > 0)
-                min = s.p;
-            if (max.compareTo(s.p) < 0)
-                max = s.p;
+        // First we get the min and max points within the segment.
+        Point min = origin;
+        Point max = origin;
+
+        for (Point p : segment) {
+            if (min.compareTo(p) > 0)
+                min = p;
+            if (max.compareTo(p) < 0)
+                max = p;
         }
 
         boolean add = true;
 
+        // Iterate over each point array in the segments array. Check if the origin point
+        // and the first point in the segment have the same slope, if they do don't add
+        // the segment. If no segment matches add the segment.
         for (Point[] p : segments) {
-            if (p[0].compareTo(min) == 0 || p[1].compareTo(max) == 0) {
+            double currentSlope = origin.slopeTo(segment.get(0));
+            double p1Slope = origin.slopeTo(p[0]);
+            double p2Slope = origin.slopeTo(p[1]);
+
+            if ((min == p[0] || Double.compare(currentSlope, p1Slope) == 0) &&
+                    (max == p[1] || Double.compare(currentSlope, p2Slope) == 0)) {
                 add = false;
                 break;
             }
@@ -143,26 +170,5 @@ public class FastCollinearPoints {
 
         if (add)
             segments.add(new Point[]{min, max});
-    }
-
-    private class SlopeObject {
-        /*
-        Class that keeps track of two points. An origin point an some other point p.
-        It also stores the slope between the two points.
-         */
-        private final Point origin;
-        private final Point p;
-        private final double slope;
-
-        SlopeObject(Point origin, Point p) {
-            this.origin = origin;
-            this.p = p;
-
-            slope = origin.slopeTo(p);
-        }
-
-        public String toString() {
-            return "O: " + origin + " P: " + p + " Slope: " + slope;
-        }
     }
 }
