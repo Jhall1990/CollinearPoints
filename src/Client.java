96fc11011b66d457943bbbb55ea3b460a7c702d3
jhall1990 <jacobhall90@gmail.com>
2019-01-10 22:22:35 -0500

    adding client changes

--- src/Client.java
+++ src/Client.java
@@ -2,8 +2,6 @@ import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdDraw;
 import edu.princeton.cs.algs4.StdOut;
 
-import java.util.Arrays;
-
 public class Client {
     public static void main(String[] args) {
 
@@ -17,34 +15,22 @@ public class Client {
 //             points[i] = new Point(x, y);
 //         }
 
-//        Point[] points = new Point[15];
-//        points[0] = new Point(10000, 0);
-//        points[1] = new Point(8000, 2000);
-//        points[2] = new Point(2000, 8000);
-//        points[3] = new Point(0, 10000);
-//        points[4] = new Point(20000, 0);
-//        points[5] = new Point(18000, 2000);
-//        points[6] = new Point(2000, 18000);
-//        points[7] = new Point(10000, 20000);
-//        points[8] = new Point(30000, 0);
-//        points[9] = new Point(0, 30000);
-//        points[10] = new Point(20000, 10000);
-//        points[11] = new Point(13000, 0);
-//        points[12] = new Point(11000, 3000);
-//        points[13] = new Point(5000, 12000);
-//        points[14] = new Point(9000, 6000);
-
-        Point[] points = new Point[10];
-        points[0] = null;
-        points[1] = null;
-        points[2] = null;
-        points[3] = null;
-        points[4] = null;
-        points[5] = null;
-        points[6] = null;
-        points[7] = null;
-        points[8] = null;
-        points[9] = null;
+        Point[] points = new Point[15];
+        points[0] =  new Point(10000, 0);
+        points[1] =  new Point(8000, 2000);
+        points[2] =  new Point(2000, 8000);
+        points[3] =  new Point(0, 10000);
+        points[4] =  new Point(20000, 0);
+        points[5] =  new Point(18000, 2000);
+        points[6] =  new Point(2000, 18000);
+        points[7] =  new Point(10000, 20000);
+        points[8] =  new Point(30000, 0);
+        points[9] =  new Point(0, 30000);
+        points[10] =  new Point(20000, 10000);
+        points[11] = new Point(13000, 0);
+        points[12] = new Point(11000, 3000);
+        points[13] = new Point(5000, 12000);
+        points[14] = new Point(9000, 6000);
 
         // draw the points
         StdDraw.enableDoubleBuffering();
@@ -56,12 +42,20 @@ public class Client {
         StdDraw.show();
 
         // print and draw the line segments
-//        FastCollinearPoints collinear = new FastCollinearPoints(points);
+        FastCollinearPoints fast = new FastCollinearPoints(points);
         BruteCollinearPoints brute = new BruteCollinearPoints(points);
+        System.out.println("Brute:");
         for (LineSegment segment : brute.segments()) {
             StdOut.println(segment);
             segment.draw();
         }
+
+        System.out.println("Fast:");
+        for (LineSegment segment : fast.segments()) {
+            StdOut.println(segment);
+            segment.draw();
+        }
+
         StdDraw.show();
     }
 }
